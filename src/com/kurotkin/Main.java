cd1e0756a08f83bebf46ae2a2ac063dd8b3106ee
kurotkin <kurotkin@users.noreply.github.com>
2017-07-10 10:49:33 +0300

    Added workers

--- src/com/kurotkin/Main.java
+++ src/com/kurotkin/Main.java
@@ -3,11 +3,13 @@ package com.kurotkin;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
+import java.util.logging.Level;
 
 
 public class Main {
 
     public static void main(String[] args) {
+        java.util.logging.Logger.getLogger("org.mongodb.driver").setLevel(Level.SEVERE);
 
         ScheduledExecutorService [] service = new ScheduledExecutorService[6];
 
@@ -16,22 +18,22 @@ public class Main {
         }
 
         service[0].scheduleAtFixedRate(new Worker("https://news.rambler.ru/rss/head/",
-                "ru_rambler_news_head"), 0, 900, TimeUnit.SECONDS);
+                "ru_rambler_news_head"), 0, 1200, TimeUnit.SECONDS);
 
         service[1].scheduleAtFixedRate(new Worker("https://news.rambler.ru/rss/world/",
-                "ru_rambler_news_world"), 100, 900, TimeUnit.SECONDS);
+                "ru_rambler_news_world"), 200, 1200, TimeUnit.SECONDS);
 
         service[2].scheduleAtFixedRate(new Worker("https://news.rambler.ru/rss/politics/",
-                "ru_rambler_news_politics"), 200, 900, TimeUnit.SECONDS);
+                "ru_rambler_news_politics"), 400, 1200, TimeUnit.SECONDS);
 
         service[3].scheduleAtFixedRate(new Worker("https://news.rambler.ru/rss/business/",
-                "ru_rambler_news_business"), 300, 900, TimeUnit.SECONDS);
+                "ru_rambler_news_business"), 600, 1200, TimeUnit.SECONDS);
 
         service[4].scheduleAtFixedRate(new Worker("https://news.rambler.ru/rss/incidents/",
-                "ru_rambler_news_incidents"), 400, 900, TimeUnit.SECONDS);
+                "ru_rambler_news_incidents"), 800, 1200, TimeUnit.SECONDS);
 
         service[5].scheduleAtFixedRate(new Worker("http://news.rambler.ru/rss/USA/",
-                "ru_rambler_news_USA"), 500, 900, TimeUnit.SECONDS);
+                "ru_rambler_news_USA"), 1000, 1200, TimeUnit.SECONDS);
 
     }
 }
--- src/com/kurotkin/Worker.java
+++ src/com/kurotkin/Worker.java
@@ -1,16 +1,18 @@
 package com.kurotkin;
 
 import com.mongodb.MongoClient;
-import com.mongodb.MongoClientURI;
-import com.mongodb.client.MongoCollection;
-import com.mongodb.client.MongoDatabase;
 import org.bson.Document;
 import org.sdecima.rssreader.RSSFeedReader;
 import org.sdecima.rssreader.RSSItem;
 import org.sdecima.rssreader.stores.ArrayListRSSFeedStore;
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.TimeUnit;
 
 /**
  * Created by Vitaly on 06.07.17.
@@ -19,24 +21,19 @@ public class Worker implements Runnable {
 
     private String urlRSS;
     private String collectionName;
+    private static DateFormat dateFormat = new SimpleDateFormat("dd.MM.yyyy HH:mm:ss");
+
+
 
     public Worker(String urlRSS, String collectionName) {
         this.urlRSS = urlRSS;
         this.collectionName = collectionName;
     }
 
-    @Override
-    public void run() {
+    public List<Document> getRSS() {
         ArrayListRSSFeedStore feedStore = new ArrayListRSSFeedStore();
         RSSFeedReader.read(urlRSS, feedStore);
-
         ArrayList<RSSItem> list = feedStore.getList();
-
-        MongoClientURI connectionString = new MongoClientURI("mongodb://127.0.0.1:27017");
-        MongoClient mongoClient = new MongoClient(connectionString);
-        MongoDatabase database = mongoClient.getDatabase("news");
-        MongoCollection<Document> collection = database.getCollection(collectionName);
-
         List<Document> documents = new ArrayList<>();
         int i = 0;
         for(RSSItem l : list) {
@@ -48,7 +45,24 @@ public class Worker implements Runnable {
             documents.add(doc);
             i++;
         }
-        System.out.println("get " + Integer.toString(i) + " from " + urlRSS);
-        collection.insertMany(documents);
+        System.out.println(dateFormat.format(new Date()) + " get " + Integer.toString(i) + " from " + urlRSS);
+        return documents;
+    }
+
+    @Override
+    public void run() {
+        final CountDownLatch insertLatch = new CountDownLatch(1);
+
+        MongoClient mongo = new MongoClient( "localhost" , 27017 );
+        mongo.getDatabase("news").getCollection(collectionName).insertMany(getRSS());
+        mongo.close();
+
+        boolean inserted = false;
+        try {
+            inserted = insertLatch.await(10, TimeUnit.SECONDS);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        assert(inserted);
     }
 }
