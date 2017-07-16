d328c32ccdbbe85d2263e36c2f6587311b702cae
kurotkin <kurotkin@users.noreply.github.com>
2017-07-16 23:09:26 +0300

    fix parser

--- src/com/kurotkin/Mongo.java
+++ src/com/kurotkin/Mongo.java
@@ -1,7 +1,56 @@
 package com.kurotkin;
 
+import com.mongodb.MongoClient;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.model.UpdateOptions;
+import org.bson.Document;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.TimeUnit;
+import java.util.logging.Level;
+
+import static com.mongodb.client.model.Filters.eq;
+
 /**
  * Created by Vitaly on 14.07.17.
  */
 public class Mongo {
+    private String host;
+    private String db;
+    private String collection;
+    private UpdateOptions options;
+
+    public Mongo(String host, String db, String collection) {
+        java.util.logging.Logger.getLogger("org.mongodb.driver").setLevel(Level.SEVERE);
+        options = new UpdateOptions();
+        options.upsert(true);
+        this.host = host;
+        this.db = db;
+        this.collection = collection;
+    }
+
+    public void update(Document doc) {
+        final CountDownLatch insertLatch = new CountDownLatch(1);
+        MongoClient mongo = new MongoClient( host , 27017 );
+        MongoCollection coll = mongo.getDatabase(db).getCollection(collection);
+
+        coll.updateOne(doc, new org.bson.Document("$set", doc), options);
+        mongo.close();
+
+        boolean inserted = false;
+        try {
+            inserted = insertLatch.await(2, TimeUnit.SECONDS);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        assert(inserted);
+    }
+
+
+    public Document req(String fuild, String value) {
+        MongoClient myMongo = new MongoClient( host , 27017 );
+        MongoCollection coll = myMongo.getDatabase(db).getCollection(collection);
+        Document doc = (Document) coll.find(eq(fuild, value)).first();
+        myMongo.close();
+        return doc;
+    }
 }
--- src/com/kurotkin/Parser.java
+++ src/com/kurotkin/Parser.java
@@ -1,44 +1,32 @@
 package com.kurotkin;
 
-import com.mongodb.MongoClient;
-import com.mongodb.client.MongoCollection;
-import com.mongodb.client.model.UpdateOptions;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import java.io.IOException;
 import java.util.Date;
-import java.util.concurrent.CountDownLatch;
-import java.util.concurrent.TimeUnit;
-import java.util.logging.Level;
 
 /**
  * Created by Vitaly Kurotkin on 12.07.2017.
  */
 public class Parser {
     public static void main(String[] args) {
-        java.util.logging.Logger.getLogger("org.mongodb.driver").setLevel(Level.SEVERE);
-        for (int i = 100000; i < 1000000; i++){
+        Mongo mongo = new Mongo("10.0.0.1", "news", "ru_tinkoff_invest_news");
+        for (int i = 45972; i < 1000000; i++) { //37804
             String url = "https://www.tinkoff.ru/invest/news/" + Integer.toString(i) +"/";
+
+            org.bson.Document docOld = mongo.req("guid", url);
+            if (docOld != null) {
+                System.out.println("ok " + i);
+                continue;
+            }
             org.bson.Document doc = getDoc(url);
+            System.out.print(doc);
+            System.out.println(" " + i);
             if (doc == null)
                 continue;
             doc.append("guid", url);
-            System.out.println(i + " }");
-            final CountDownLatch insertLatch = new CountDownLatch(1);
-            MongoClient mongo = new MongoClient( "10.0.0.1" , 27017 );
-            MongoCollection coll = mongo.getDatabase("news").getCollection("ru_tinkoff_invest_news");
-            UpdateOptions options = new UpdateOptions();
-            options.upsert(true);
-            coll.updateOne(doc, new org.bson.Document("$set", doc), options);
-            mongo.close();
-
-            boolean inserted = false;
-            try {
-                inserted = insertLatch.await(2, TimeUnit.SECONDS);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-            assert(inserted);
+            System.out.println(i);
+            mongo.update(doc);
         }
     }
 
@@ -58,9 +46,6 @@ public class Parser {
         String name = doc.getElementsByClass("_3WKsv _2yX9u").text();
         String sd = doc.getElementsByClass("_3WKsv _3zKqJ iaS10").text();
         String s = doc.getElementsByClass("_1KGyb").text();
-
-        System.out.println(dat);
-        System.out.println(name);
 
         org.bson.Document docBson = new org.bson.Document("Date", date)
                 .append("title", name)
